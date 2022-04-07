e27f9eeb56167fd77b4dfdc9e8cf4a19449ef5df
Federico Turban <fturbana@hotmail.com>
2022-04-06 21:28:53 -0500

    First commit

---
+++ tests/browserdriver.js
@@ -0,0 +1,32 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function helloSelenium() {
+    let driver = await new Builder().forBrowser('MicrosoftEdge').build();
+
+    await driver.get('http://automationpractice.com/index.php');
+
+    await driver.getTitle(); // => "My Store"
+
+    driver.manage().setTimeouts({implicit: 0.5 })
+
+    let goToSignInButton = await driver.findElement(By.className('login'));
+    //let emailBox = await driver.findElement(By.id('email'));
+    //let passwordBox = await driver.findElement(By.id('passwd'));
+    //let signInButton = await driver.findElement(By.id('SubmitLogin'));
+
+    await goToSignInButton.click();
+    driver.manage().setTimeouts({implicit: 1.5 })
+    //await emailBox.sendKeys('fturbana@hotmail.com');
+    //driver.manage().setTimeouts({implicit: 0.5 })
+    //await passwordBox.sendKeys('123456');
+    //driver.manage().setTimeouts({implicit: 0.5 })
+    //await signInButton.click();
+
+    //driver.manage().setTimeouts({implicit: 0.5 })
+
+    //await searchButton.click();
+
+    await driver.findElement(By.className('page-heading')).getText("AUTHENTICATION"); // => 'Selenium'
+
+    await driver.quit();
+})();
