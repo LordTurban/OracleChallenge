28233d1b9c1fc45154b6fa357e01b43be12845b4
Federico Turban <fturbana@hotmail.com>
2022-04-07 02:40:57 -0500

    Second Commit

--- tests/browserdriver.js
+++ tests/browserdriver.js
@@ -1,6 +1,6 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function helloSelenium() {
+(async function mainTest() {
     let driver = await new Builder().forBrowser('MicrosoftEdge').build();
 
     await driver.get('http://automationpractice.com/index.php');
@@ -10,23 +10,24 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     driver.manage().setTimeouts({implicit: 0.5 })
 
     let goToSignInButton = await driver.findElement(By.className('login'));
-    //let emailBox = await driver.findElement(By.id('email'));
-    //let passwordBox = await driver.findElement(By.id('passwd'));
-    //let signInButton = await driver.findElement(By.id('SubmitLogin'));
+    let emailBox = await driver.findElement(By.id('email'));
+    let passwordBox = await driver.findElement(By.id('passwd'));
+    let signInButton = await driver.findElement(By.id('SubmitLogin'));
 
     await goToSignInButton.click();
     driver.manage().setTimeouts({implicit: 1.5 })
-    //await emailBox.sendKeys('fturbana@hotmail.com');
-    //driver.manage().setTimeouts({implicit: 0.5 })
-    //await passwordBox.sendKeys('123456');
-    //driver.manage().setTimeouts({implicit: 0.5 })
-    //await signInButton.click();
+    await emailBox.sendKeys('fturbana@hotmail.com');
+    driver.manage().setTimeouts({implicit: 0.5 })
+    await passwordBox.sendKeys('123456');
+    driver.manage().setTimeouts({implicit: 0.5 })
+    await signInButton.click();
 
-    //driver.manage().setTimeouts({implicit: 0.5 })
+    driver.manage().setTimeouts({implicit: 0.5 })
 
     //await searchButton.click();
 
-    await driver.findElement(By.className('page-heading')).getText("AUTHENTICATION"); // => 'Selenium'
+    //await driver.findElement(By.className('page-heading')).getText("AUTHENTICATION");
+    
 
     await driver.quit();
 })();
